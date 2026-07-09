79c7178fb22f2f4e3af63488bc02e22c9ad4e224
thomasgggg <89398918+thomasgggg@users.noreply.github.com>
2026-07-09 11:04:10 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CanteiroDashboard from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CanteiroDashboard />
+  </React.StrictMode>
+);
